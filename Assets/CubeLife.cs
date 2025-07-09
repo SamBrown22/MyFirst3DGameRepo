8ca1dc7e46b41685ff6956184aaa996f8411e9d0
SamBrown22 <samuelstevenbrown@gmail.com>
2025-07-09 18:40:00 +0100

    test changes

--- Assets/CubeLife.cs
+++ Assets/CubeLife.cs
@@ -2,13 +2,13 @@ using UnityEngine;
 
 public class CubeLife : MonoBehaviour
 {
-    public string targetTag = "Bullet";
+    public string targetTag = "Bullet"; //test change
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag(targetTag))
         {
-            Destroy(gameObject); // Destroys the cube itself
+            Destroy(gameObject); // Destroys the cube itself 
         }
     }
 }
